1d4b269224821f63a34e71eafaf570656eb3f974
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-12 14:30:31 -0300

    Formatacao concluida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Components
+import { SignComponent } from './core/components/pages/sign/sign.component';
+import { HomeComponent } from './components/admin/pages/home/home.component';
+
+export const routes: Routes = [
+  { path: '', component: SignComponent },
+  { path: 'admin', component: HomeComponent },
+];
