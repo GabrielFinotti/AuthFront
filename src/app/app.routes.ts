b67eb64eba95fc1ca1f3336ccdc81f4ec85f3f37
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-15 18:15:08 -0300

    Router Guard
    Criando a autenticação da rota fazendo a verificação do token de usuário!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 
+// Guards
+import { authGuard } from './core/guards/auth.guard';
+
 // Components
 import { SignComponent } from './core/components/pages/sign/sign.component';
 import { HomeComponent } from './components/admin/pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: SignComponent },
-  { path: 'admin', component: HomeComponent },
+  { path: 'admin', component: HomeComponent, canActivate: [authGuard] },
 ];
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
+// Serviços
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -37,5 +40,16 @@ export class AuthService {
     localStorage.removeItem('access_token');
 
     return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token')
+
+    if(!token) {
+      return false
+    } else {
+      const jwtHelper = new JwtHelperService();
+      return !jwtHelper.isTokenExpired(token);
+    }
   }
 }
